2ea33916281610742b22076f1b27e52438d65877
Tan Mai <tanmai@kms-technology.com>
2018-11-18 12:32:46 +0700

    add new code for backend

--- reactjs-frontend/src/modules/productDetail/actionTypes.js
+++
@@ -1,1 +0,0 @@
-export const GET_PRODUCT_DETAIL = 'GET_PRODUCT_DETAIL';
--- reactjs-frontend/src/rootReducer.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from 'redux';
-import productList from './modules/productList/reducer';
-import productDetail from './modules/productDetail/reducer';
-
-export default combineReducers({ productList, productDetail });
